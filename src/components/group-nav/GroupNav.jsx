39b738cbd0459760042151a2d2ccdbdd06a213d4
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-06-20 12:44:50 +0200

    add user button

--- src/components/group-nav/GroupNav.jsx
+++ src/components/group-nav/GroupNav.jsx
@@ -1,4 +1,6 @@
 import { Link, useLocation, useParams } from 'react-router-dom';
+import { AiOutlineUserAdd } from 'react-icons/Ai';
+import Button from '../button/Button';
 import './group-nav.scss';
 
 const GroupNav = () => {
@@ -8,24 +10,38 @@ const GroupNav = () => {
 
   return (
     <section className="group-nav">
-      <Link
-        to={`/groups/${groupId}/tasks`}
-        className={`group-nav__link ${activePage === 'tasks' && 'group-nav__link-active'}`}
+      <section className="group-nav__container">
+        <Link
+          to={`/groups/${groupId}/tasks`}
+          className={`group-nav__link ${activePage === 'tasks' && 'group-nav__link-active'}`}
+        >
+          Tasks
+        </Link>
+        <Link
+          to={`/groups/${groupId}/calendar`}
+          className={`group-nav__link ${activePage === 'calendar' && 'group-nav__link-active'}`}
+        >
+          Calendar
+        </Link>
+        <Link
+          to={`/groups/${groupId}/chat`}
+          className={`group-nav__link ${activePage === 'chat' && 'group-nav__link-active'}`}
+        >
+          Chat
+        </Link>
+      </section>
+      <Button
+        style={{
+          padding: '0 0.75rem',
+          display: 'flex',
+          gap: '0.25rem',
+          alignItems: 'center',
+          marginRight: '5rem',
+        }}
       >
-        Tasks
-      </Link>
-      <Link
-        to={`/groups/${groupId}/calendar`}
-        className={`group-nav__link ${activePage === 'calendar' && 'group-nav__link-active'}`}
-      >
-        Calendar
-      </Link>
-      <Link
-        to={`/groups/${groupId}/chat`}
-        className={`group-nav__link ${activePage === 'chat' && 'group-nav__link-active'}`}
-      >
-        Chat
-      </Link>
+        <AiOutlineUserAdd />
+        Add User
+      </Button>
     </section>
   );
 };
