152bf45e1d95aab80c7d4ed0db541931f7388bff
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-07-11 20:07:24 +0200

    improvved calendar event design

--- src/components/calendar-item/CalendarItem.jsx
+++ src/components/calendar-item/CalendarItem.jsx
@@ -10,23 +10,18 @@ const CalendarItem = ({ currentMonth, day, isSunday, isToday, events }) => {
   const closePopup = () => setIsPopUpOpen(false);
 
   return (
-    <>
-      {currentMonth ? (
-        <section className={`calendar-item ${isToday ? 'calendar-item__selected' : ''}`}>
-          <p style={{ color: isSunday && 'var(--clr-red)' }}>{day}</p>
-          <AiOutlinePlus className="calendar-item__add-event" onClick={openPopup} />
-          <section className="calendar-item__events">
-            <CalendarEvent />
-            <CalendarEvent />
-            <CalendarEvent />
-          </section>
-        </section>
-      ) : (
-        <section className="calendar-item" style={{ opacity: !currentMonth && 0.8 }}>
-          <p style={{ color: isSunday && 'var(--clr-red)' }}>{day}</p>
-        </section>
-      )}
-    </>
+    <section
+      className={`calendar-item ${isToday ? 'calendar-item__selected' : ''}`}
+      style={{ opacity: !currentMonth && 0.8 }}
+    >
+      <p style={{ color: isSunday && 'var(--clr-red)' }}>{day}</p>
+      <AiOutlinePlus className="calendar-item__add-event" onClick={openPopup} />
+      <section className="calendar-item__events">
+        <CalendarEvent />
+        <CalendarEvent />
+        <CalendarEvent />
+      </section>
+    </section>
   );
 };
 
