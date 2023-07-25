29a9fb22b611b5afb9027e98ed5f025a99e369f9
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-07-25 11:23:23 +0200

    first chat design finished with message component

--- src/components/chat-container/ChatContainer.jsx
+++ src/components/chat-container/ChatContainer.jsx
@@ -1,7 +1,19 @@
 import './chat-container.scss';
+import Message from '../message/Message';
 
 const ChatContainer = () => {
-  return <div className="chat-container">ChatContainer</div>;
+  return (
+    <div className="chat-container">
+      <Message displayName={'kacper'} content={'Very long ngbouasgj gdgs'} />
+      <Message
+        displayName={'Ktos inny'}
+        content={
+          'Very long Lorem ipsum hello hello what are you doing here whats up and here we go lets get to three lines '
+        }
+      />
+      <Message isYours={true} displayName={'Test User'} content={'Very long message'} />
+    </div>
+  );
 };
 
 export default ChatContainer;
---
+++ src/components/message/Message.jsx
@@ -0,0 +1,26 @@
+import './message.scss';
+
+const Message = ({ isYours = false, displayName, content }) => {
+  return (
+    <section
+      className="message"
+      style={{ justifyContent: isYours && 'flex-end', marginLeft: isYours && 'auto' }}
+    >
+      {!isYours && (
+        <section className="message__profile">
+          <img
+            src="/profile-picture.png"
+            alt="profile picture"
+            className="message__profile-picture"
+          />
+          <p className="message__display-name">{displayName}</p>
+        </section>
+      )}
+      <section className="message__content-container">
+        <p className="message__content">{content}</p>
+      </section>
+    </section>
+  );
+};
+
+export default Message;
