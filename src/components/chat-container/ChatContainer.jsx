b32a1845ef846b2c65e39d9768609181eb34b5ce
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-07-25 11:46:12 +0200

    basic fetching messages without redux

--- src/components/chat-container/ChatContainer.jsx
+++ src/components/chat-container/ChatContainer.jsx
@@ -1,17 +1,32 @@
 import './chat-container.scss';
 import Message from '../message/Message';
+import { useParams } from 'react-router';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { getDocumentRef } from '../../utils/firebase/firebase';
+import { getCurrentUser } from '../../features/user/userSlice';
+import { useSelector } from 'react-redux';
+import { ImSpinner2 } from 'react-icons/im';
 
 const ChatContainer = () => {
+  const { groupId } = useParams();
+  const currentUser = useSelector(getCurrentUser);
+  const [data = {}, loading, error] = useDocumentData(getDocumentRef('chat', groupId));
+
   return (
-    <div className="chat-container">
-      <Message displayName={'kacper'} content={'Very long ngbouasgj gdgs'} />
-      <Message
-        displayName={'Ktos inny'}
-        content={
-          'Very long Lorem ipsum hello hello what are you doing here whats up and here we go lets get to three lines '
-        }
-      />
-      <Message isYours={true} displayName={'Test User'} content={'Very long message'} />
+    <div className="chat-container" style={{ placeContent: (loading || error) && 'center' }}>
+      {loading ? (
+        <ImSpinner2 className="spinner" style={{ margin: ' 0 auto' }} />
+      ) : (
+        data.messages.map(({ uid, displayName, content }, i) => (
+          <Message
+            isYours={uid === currentUser.uid}
+            displayName={displayName}
+            content={content}
+            key={i}
+          />
+        ))
+      )}
+      {error && <p className="chat-container__error">Error. Please refresh the page.</p>}
     </div>
   );
 };
