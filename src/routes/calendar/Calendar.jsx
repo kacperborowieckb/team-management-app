25bd1a300b68fc55e5575d631d486870e08dc6b4
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-08-01 15:25:40 +0200

    fixed header layout

--- src/routes/calendar/Calendar.jsx
+++ src/routes/calendar/Calendar.jsx
@@ -22,6 +22,7 @@ const Calendar = () => {
   const currentDate = useSelector(getCurrentDate);
   const yearAndMonth = `${currentDate.year}${currentDate.month}`;
   const events = useSelector(getCurrentEvents);
+
   const handleIncreaseMonth = () => dispatch(increaseMonth());
   const handleDecreaseMonth = () => dispatch(decreaseMonth());
 
